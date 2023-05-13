041a62e4753c9ad0a0a1308bb70fb948b3a41ac1
vinisi12363 <viniciuspv.si@gmail.com>
2023-05-13 20:24:37 -0300

    ajustes

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,7 +1,6 @@
 import { Router } from "express"
-import {getCustomers, insertCustomers, updateCustomers} from '../controllers/customer.controller;.js'
+import {getCustomers, insertCustomers, updateCustomers} from '../controllers/customer.controller.js'
 import { validateCustomerSchema } from "../middlewares/validateUserSchema.middleware.js"
-import {searchCustomer} from '../middlewares/searchCustomerAlreadyExist.middleware.js'
 import {customersSchema} from '../schemas/customer.schema.js'
 
 const customerRouter = Router()
